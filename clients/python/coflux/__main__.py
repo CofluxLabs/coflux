5b4b1850ce59088e9794b4549cef31463b595749
Joe Freeman <joe@joef.uk>
2021-12-10 20:30:22 +0000

    Automatically reload workers

--- clients/python/coflux/__main__.py
+++ clients/python/coflux/__main__.py
@@ -1,17 +1,35 @@
 import click
 import asyncio
+import watchgod
 
 from . import Client
 
 
+def _callback(changes):
+    print("Change detected. Restarting...")
+
+
+def _run(project, module, version, host, concurrency):
+    try:
+        client = Client(project, module, version, host, concurrency)
+        asyncio.run(client.run())
+    except KeyboardInterrupt:
+        pass
+
+
 @click.command()
-@click.option('-p', '--project', required=True)
-@click.option('-m', '--module', required=True)
-@click.option('-v', '--version', required=True)
-@click.option('-h', '--host', required=True)
-def cli(project, module, version, host):
-    client = Client(project, module, version, host)
-    asyncio.run(client.run())
+@click.option('-p', '--project', required=True, help="Project ID")
+@click.option('-m', '--module', required=True, help="Python module to use")
+@click.option('-v', '--version', required=True, help="Version identifier to report to the server")
+@click.option('-h', '--host', required=True, help="Host to connect to")
+@click.option('--concurrency', type=int, help="Limit on number of executions to process at once")
+@click.option('--reload', is_flag=True, default=False, help="Enable auto-reload when code changes")
+def cli(project, module, version, host, concurrency, reload):
+    args = (project, module, version, host, concurrency)
+    if reload:
+        watchgod.run_process('.', _run, args=args, callback=_callback)
+    else:
+        _run(*args)
 
 
 cli()
