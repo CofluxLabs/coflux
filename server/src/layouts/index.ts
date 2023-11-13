edf34eb39b8274ef727c2092ed9a473942a7f1cb
Joe Freeman <joe@joef.uk>
2023-11-13 21:55:42 +0000

    Remove lazy loading

---
+++ server/src/layouts/index.ts
@@ -0,0 +1,3 @@
+export { default as ExternalLayout } from "./ExternalLayout";
+export { default as ProjectLayout } from "./ProjectLayout";
+export { default as RunLayout } from "./RunLayout";
---
+++ server/src/pages/index.ts
@@ -0,0 +1,8 @@
+export { default as HomePage } from "./HomePage";
+export { default as ProjectPage } from "./ProjectPage";
+export { default as ProjectsPage } from "./ProjectsPage";
+export { default as GraphPage } from "./GraphPage";
+export { default as TimelinePage } from "./TimelinePage";
+export { default as LogsPage } from "./LogsPage";
+export { default as TaskPage } from "./TaskPage";
+export { default as SensorPage } from "./SensorPage";
