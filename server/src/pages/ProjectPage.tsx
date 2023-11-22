fb4328e4762fa175076dc641de0b15f0397cbb4c
Joe Freeman <joe@joef.uk>
2023-11-22 18:54:29 +0000

    Redirect to environment

--- server/src/pages/ProjectPage.tsx
+++ server/src/pages/ProjectPage.tsx
@@ -1,3 +1,19 @@
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { useTopic } from "@topical/react";
+
+import * as models from "../models";
+
 export default function ProjectPage() {
+  const { project: projectId } = useParams();
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const environmentName = searchParams.get("environment") || undefined;
+  const [projects] = useTopic<Record<string, models.Project>>("projects");
+  if (projectId && projects && !environmentName) {
+    const environment = projects[projectId].environments[0];
+    if (environment) {
+      navigate(`/projects/${projectId}?environment=${environment}`);
+    }
+  }
   return <div></div>;
 }
