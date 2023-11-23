a3447e3403242ce51d870504402020bd459dd485
Joe Freeman <joe@joef.uk>
2023-11-23 21:16:24 +0000

    Start implementing projects page

--- server/src/pages/ProjectsPage.tsx
+++ server/src/pages/ProjectsPage.tsx
@@ -1,3 +1,46 @@
+import { useTopic } from "@topical/react";
+import { Link } from "react-router-dom";
+
+import * as models from "../models";
+import { IconBox, IconPlus, IconPyramid } from "@tabler/icons-react";
+
 export default function ProjectsPage() {
-  return <div></div>;
+  const [projects] = useTopic<Record<string, models.Project>>("projects");
+  return (
+    <div>
+      {projects && (
+        <ul className="flex flex-wrap gap-4 p-4">
+          {Object.keys(projects).map((projectId) => (
+            <li key={projectId} className="flex w-44 h-44">
+              <Link
+                to={`/projects/${projectId}`}
+                className="group flex-1 flex gap-2 flex-col justify-center p-4 pt-8 border border-slate-200 rounded-lg items-center hover:border-slate-300 hover:shadow hover:bg-slate-100/20"
+              >
+                <IconPyramid
+                  size={50}
+                  stroke={1.5}
+                  className="text-slate-500 group-hover:text-slate-700"
+                />
+                <h3 className="font-mono font-bold text-slate-500 group-hover:text-slate-600">
+                  {projectId}
+                </h3>
+              </Link>
+            </li>
+          ))}
+          <li className="flex w-44 h-44">
+            <button className="group flex-1 flex gap-2 flex-col justify-center p-4 pt-8 border border-dashed border-slate-200 rounded-lg items-center hover:border-slate-300 hover:bg-slate-100/20">
+              <IconPlus
+                size={50}
+                stroke={1.5}
+                className="text-slate-500 group-hover:text-slate-700"
+              />
+              <h3 className="text-slate-500 group-hover:text-slate-600">
+                New project...
+              </h3>
+            </button>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
 }
