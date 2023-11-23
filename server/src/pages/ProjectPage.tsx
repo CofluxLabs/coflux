1a10fed89f0915a6d27e113798925f03835e4de8
Joe Freeman <joe@joef.uk>
2023-11-23 21:16:06 +0000

    Use effect for redirect

--- server/src/pages/ProjectPage.tsx
+++ server/src/pages/ProjectPage.tsx
@@ -2,18 +2,22 @@ import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import { useTopic } from "@topical/react";
 
 import * as models from "../models";
+import { useEffect } from "react";
 
 export default function ProjectPage() {
   const { project: projectId } = useParams();
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
-  const environmentName = searchParams.get("environment") || undefined;
+  const currentEnvironment = searchParams.get("environment") || undefined;
   const [projects] = useTopic<Record<string, models.Project>>("projects");
-  if (projectId && projects && !environmentName) {
-    const environment = projects[projectId].environments[0];
-    if (environment) {
-      navigate(`/projects/${projectId}?environment=${environment}`);
+  const defaultEnvironment =
+    projectId && projects && projects[projectId]?.environments[0];
+  useEffect(() => {
+    if (projectId && !currentEnvironment && defaultEnvironment) {
+      navigate(`/projects/${projectId}?environment=${defaultEnvironment}`, {
+        replace: true,
+      });
     }
-  }
+  }, [navigate, projectId, currentEnvironment, defaultEnvironment]);
   return <div></div>;
 }
