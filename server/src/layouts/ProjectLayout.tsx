27f5f7040ecac417891133a3701504327bf70ee9
Joe Freeman <joe@joef.uk>
2023-12-17 20:54:09 +0000

    Show steps for getting started

--- server/src/layouts/ProjectLayout.tsx
+++ server/src/layouts/ProjectLayout.tsx
@@ -72,6 +72,22 @@ function ConnectionStatus({
   );
 }
 
+type CodeBlockProps = {
+  code: string;
+};
+
+function CodeBlock({ code }: CodeBlockProps) {
+  return (
+    <code className="block whitespace-pre bg-white shadow-inner rounded-md p-2 my-1">
+      {code}
+    </code>
+  );
+}
+
+function generatePackageName(projectName: string | undefined) {
+  return projectName?.replace(/[^a-zA-Z0-9_]/g, "") || "my_package";
+}
+
 type OutletContext = {
   setActiveTarget: (task: Target | undefined) => void;
 };
@@ -94,8 +110,8 @@ export default function ProjectLayout() {
     "agents"
   );
   const currentEnvironment = searchParams.get("environment") || undefined;
-  const defaultEnvironment =
-    projectId && projects && projects[projectId]?.environments[0];
+  const project = (projectId && projects && projects[projectId]) || undefined;
+  const defaultEnvironment = project?.environments[0];
   useEffect(() => {
     if (projectId && !currentEnvironment && defaultEnvironment) {
       // TODO: retain current url?
@@ -107,7 +123,39 @@ export default function ProjectLayout() {
   if (!repositories) {
     return <Loading />;
   } else if (!Object.keys(repositories).length) {
-    return <div></div>;
+    const packageName = generatePackageName(project?.name);
+    return (
+      <div className="bg-slate-50 border border-slate-100 rounded-lg m-auto w-2/3 p-3 text-slate-600">
+        <h1 className="text-3xl my-2">Your project is ready</h1>
+        <p className="my-2">
+          Next, connect your agent. If you don't already have one setup, here's
+          what you can do:
+        </p>
+        <ol className="list-decimal pl-8 my-4">
+          <li className="my-4">
+            Install the Python client:
+            <CodeBlock code="pip install coflux" />
+          </li>
+          <li className="my-4">
+            Create a config file and initialise the Python packge:
+            <CodeBlock
+              code={`coflux init \\\n  --host=${window.location.host} \\\n  --project=${projectId} \\\n  --environment=${environmentName} \\\n  --repo=${packageName}.repo`}
+            />
+          </li>
+          <li className="my-4">
+            Run the agent (watching for changes):
+            <CodeBlock code={`coflux agent.run ${packageName}.repo --reload`} />
+          </li>
+          <li className="my-4">
+            Edit <code>{`${packageName}/repo.py`}</code> to add a task to your
+            repository:
+            <CodeBlock
+              code={`import coflux\n\n@coflux.task()\ndef hello(name: str):\n    coflux.context.log_info(f"Hello, {name}")\n    return 42`}
+            />
+          </li>
+        </ol>
+      </div>
+    );
   } else {
     return (
       <div className="flex-auto flex overflow-hidden">
