6cc378cdfc08cb1d0e2423c68bc5414367a9e4de
Joe Freeman <joe@joef.uk>
2023-12-17 16:31:32 +0000

    Redirect to projects page

--- server/src/pages/HomePage.tsx
+++ server/src/pages/HomePage.tsx
@@ -1,10 +1,8 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <Link to="/projects">Projects</Link>
-    </div>
-  );
+  const navigate = useNavigate();
+  useEffect(() => navigate("/projects", { replace: true }), [navigate]);
+  return <div></div>;
 }
