e7919a4016b9bae2788aef466fd2fd7cb82fa1ac
Joe Freeman <joe@joef.uk>
2024-02-12 15:33:48 +0000

    Remove 'python repo' example

--- examples/python_repo/example/__main__.py
+++
@@ -1,4 +0,0 @@
-import coflux
-from . import repo
-
-coflux.init(repo)
--- examples/python_repo/shell.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.python311
-    pkgs.poetry
-  ];
-}
