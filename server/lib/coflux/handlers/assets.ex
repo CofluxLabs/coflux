9da6750213676d0f14389ef476756ab437221123
Joe Freeman <joe@joef.uk>
2024-09-15 14:46:35 +0100

    Refactor assets handler

--- server/lib/coflux/handlers/assets.ex
+++ server/lib/coflux/handlers/assets.ex
@@ -22,73 +22,98 @@ defmodule Coflux.Handlers.Assets do
         {:ok, asset_type, _path, blob_key, metadata} ->
           cond do
             asset_type == 0 && path == [] ->
-              content_type = Map.get(metadata, "type", "application/octet-stream")
-
-              case File.read(blob_path(blob_key)) do
-                {:ok, content} ->
-                  :cowboy_req.reply(
-                    200,
-                    %{"content-type" => content_type},
-                    content,
-                    req
-                  )
-              end
+              file_asset(blob_key, metadata, req)
 
             asset_type == 1 ->
-              {:ok, unzip} =
-                blob_key
-                |> blob_path()
-                |> Unzip.LocalFile.open()
-                |> Unzip.new()
-
-              path = Enum.join(path, "/")
-
-              paths =
-                unzip
-                |> Unzip.list_entries()
-                |> Map.new(&{&1.file_name, &1.uncompressed_size})
-
-              cond do
-                path == "" ->
-                  :cowboy_req.reply(
-                    200,
-                    %{"content-type" => "application/json"},
-                    Jason.encode!(
-                      Enum.map(paths, fn {path, size} ->
-                        %{path: path, size: size, type: MIME.from_path(path)}
-                      end)
-                    ),
-                    req
-                  )
-
-                Map.has_key?(paths, path) ->
-                  mime_type = MIME.from_path(path)
-                  req = :cowboy_req.stream_reply(200, %{"content-type" => mime_type}, req)
-
-                  unzip
-                  |> Unzip.file_stream!(path)
-                  |> Stream.each(fn part ->
-                    :cowboy_req.stream_body(part, :nofin, req)
-                  end)
-                  |> Stream.run()
-
-                  :cowboy_req.stream_body([], :fin, req)
-
-                  req
-
-                true ->
-                  :cowboy_req.reply(404, %{}, "Not found", req)
-              end
+              directory_asset(blob_key, path, req)
 
             true ->
-              :cowboy_req.reply(404, %{}, "Not found", req)
+              not_found(req)
           end
 
         {:error, _error} ->
-          :cowboy_req.reply(404, %{}, "Not found", req)
+          not_found(req)
       end
 
     {:ok, req, opts}
+  end
+
+  defp file_asset(blob_key, metadata, req) do
+    content_type = Map.get(metadata, "type", "application/octet-stream")
+
+    case File.read(blob_path(blob_key)) do
+      {:ok, content} ->
+        :cowboy_req.reply(
+          200,
+          %{"content-type" => content_type},
+          content,
+          req
+        )
+    end
+  end
+
+  defp directory_asset(blob_key, path, req) do
+    {:ok, unzip, paths} = load_zip(blob_key)
+    path = Enum.join(path, "/")
+
+    cond do
+      path == "" ->
+        directory_index(paths, req)
+
+      Map.has_key?(paths, path) ->
+        directory_file(unzip, path, req)
+
+      true ->
+        not_found(req)
+    end
+  end
+
+  defp load_zip(blob_key) do
+    {:ok, unzip} =
+      blob_key
+      |> blob_path()
+      |> Unzip.LocalFile.open()
+      |> Unzip.new()
+
+    paths =
+      unzip
+      |> Unzip.list_entries()
+      |> Map.new(&{&1.file_name, &1.uncompressed_size})
+
+    {:ok, unzip, paths}
+  end
+
+  defp directory_index(paths, req) do
+    :cowboy_req.reply(
+      200,
+      %{"content-type" => "application/json"},
+      Jason.encode!(
+        Enum.map(paths, fn {path, size} ->
+          %{path: path, size: size, type: MIME.from_path(path)}
+        end)
+      ),
+      req
+    )
+  end
+
+  defp directory_file(unzip, path, req) do
+    mime_type = MIME.from_path(path)
+    req = :cowboy_req.stream_reply(200, %{"content-type" => mime_type}, req)
+
+    unzip
+    |> Unzip.file_stream!(path)
+    |> Stream.each(fn part ->
+      :cowboy_req.stream_body(part, :nofin, req)
+    end)
+    |> Stream.run()
+
+    :cowboy_req.stream_body([], :fin, req)
+
+    req
+  end
+
+  defp not_found(req) do
+    :cowboy_req.reply(404, %{}, "Not found", req)
   end
 
   defp blob_path(<<a::binary-size(2), b::binary-size(2)>> <> c) do
