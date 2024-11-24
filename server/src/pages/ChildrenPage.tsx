22f1bf4a4ad09cf374b0c689fea0b75965692879
Joe Freeman <joe@joef.uk>
2024-11-24 17:44:38 +0000

    Handle no children

--- server/src/pages/ChildrenPage.tsx
+++ server/src/pages/ChildrenPage.tsx
@@ -13,74 +13,81 @@ export default function ChildrenPage() {
     (stepId) => !run.steps[stepId].parentId,
   )!;
   const executions = run.steps[initialStepId].executions;
+  const hasChildren = Object.values(executions).some((e) =>
+    e.children.some((c) => typeof c != "string"),
+  );
   return (
     <div className="p-4">
-      <table className="w-full">
-        <tbody>
-          {Object.keys(executions)
-            .map((a) => parseInt(a, 10))
-            .sort()
-            .map((attempt) => {
-              const children = executions[attempt].children.filter(
-                (c): c is models.Child => typeof c != "string",
-              );
-              return (
-                <Fragment key={attempt}>
-                  {children.length ? (
-                    <Fragment>
-                      <tr>
-                        <td colSpan={2}>
-                          <h3 className="text-xs text-slate-400 uppercase font-semibold mt-3 mb-1">
-                            <StepLink
-                              runId={runId!}
-                              stepId={initialStepId}
-                              attempt={attempt}
-                              className="rounded ring-offset-1 px-1"
-                              activeClassName="ring-2 ring-cyan-400"
-                              hoveredClassName="ring-2 ring-slate-300"
-                            >
-                              Iteration #{attempt}
-                            </StepLink>
-                          </h3>
-                        </td>
-                      </tr>
-                      {children.map((child) => {
-                        const createdAt = DateTime.fromMillis(run.createdAt);
-                        return (
-                          <tr key={child.runId}>
-                            <td>
-                              <span className="text-sm text-slate-400">
-                                {createdAt.toLocaleString(
-                                  DateTime.DATETIME_SHORT_WITH_SECONDS,
-                                )}
-                              </span>
-                            </td>
-                            <td>
+      {hasChildren ? (
+        <table className="w-full">
+          <tbody>
+            {Object.keys(executions)
+              .map((a) => parseInt(a, 10))
+              .sort()
+              .map((attempt) => {
+                const children = executions[attempt].children.filter(
+                  (c): c is models.Child => typeof c != "string",
+                );
+                return (
+                  <Fragment key={attempt}>
+                    {children.length ? (
+                      <Fragment>
+                        <tr>
+                          <td colSpan={2}>
+                            <h3 className="text-xs text-slate-400 uppercase font-semibold mt-3 mb-1">
                               <StepLink
-                                runId={child.runId}
-                                stepId={child.stepId}
-                                attempt={1}
-                                className="rounded text-sm ring-offset-1 px-1"
+                                runId={runId!}
+                                stepId={initialStepId}
+                                attempt={attempt}
+                                className="rounded ring-offset-1 px-1"
+                                activeClassName="ring-2 ring-cyan-400"
                                 hoveredClassName="ring-2 ring-slate-300"
                               >
-                                <span className="font-mono">
-                                  {child.target}
-                                </span>{" "}
-                                <span className="text-slate-500 text-xs">
-                                  ({child.repository})
-                                </span>
+                                Iteration #{attempt}
                               </StepLink>
-                            </td>
-                          </tr>
-                        );
-                      })}
-                    </Fragment>
-                  ) : null}
-                </Fragment>
-              );
-            })}
-        </tbody>
-      </table>
+                            </h3>
+                          </td>
+                        </tr>
+                        {children.map((child) => {
+                          const createdAt = DateTime.fromMillis(run.createdAt);
+                          return (
+                            <tr key={child.runId}>
+                              <td>
+                                <span className="text-sm text-slate-400">
+                                  {createdAt.toLocaleString(
+                                    DateTime.DATETIME_SHORT_WITH_SECONDS,
+                                  )}
+                                </span>
+                              </td>
+                              <td>
+                                <StepLink
+                                  runId={child.runId}
+                                  stepId={child.stepId}
+                                  attempt={1}
+                                  className="rounded text-sm ring-offset-1 px-1"
+                                  hoveredClassName="ring-2 ring-slate-300"
+                                >
+                                  <span className="font-mono">
+                                    {child.target}
+                                  </span>{" "}
+                                  <span className="text-slate-500 text-xs">
+                                    ({child.repository})
+                                  </span>
+                                </StepLink>
+                              </td>
+                            </tr>
+                          );
+                        })}
+                      </Fragment>
+                    ) : null}
+                  </Fragment>
+                );
+              })}
+          </tbody>
+        </table>
+      ) : (
+        <p className="italic">None</p>
+      )}
     </div>
   );
 }
